defe70ca8a1562f4dfd1e128203f0a2393b676ba
Travis Meamber <travis.meamber@nvih.org>
2019-12-18 10:52:09 -0800

    Just goofing around

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,29 @@
-console.log('Hello World?!?!');
+var firstName = "Travis";
+console.log(firstName);
+
+let lastName ='Meamber';
+let age = 25;
+
+let fullAge = true;
+
+console.log(fullAge);
+
+let job;
+job = 'Teacher';
+console.log(job);
+
+let $3years = 3; //(Will show up as invalid so it's null you can use a $ sign, underscore or a letter, to get this to work) 
+
+
+
+
+
+//5 Different Data Types 
+//1) number: floating pointing numbers, for decimals or integers.
+//2) String: Sequence of characters; used for text
+//3) Boolean: logical data type that can only be true or false
+//4) Undefined: Data type of variable that does not have a value yet.
+//5) Null: Non-existant.
+
+
+//cant use reserved javascript keywords as variables they wil NOT work.
