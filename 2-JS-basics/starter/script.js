4e690525fa969320d268874219eb9ad9bc64a51e
Travis Meamber <travis.meamber@nvih.org>
2019-12-18 11:23:11 -0800

    variable mutation and type correcion

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,16 +1,16 @@
 var firstName = "Travis";
 console.log(firstName);
 
-let lastName ='Meamber';
+// let lastName ='Meamber';
 let age = 25;
 
 let fullAge = true;
 
 console.log(fullAge);
 
-let job;
-job = 'Teacher';
-console.log(job);
+// let job;
+// job = 'Teacher';
+// console.log(job);
 
 let $3years = 3; //(Will show up as invalid so it's null you can use a $ sign, underscore or a letter, to get this to work) 
 
@@ -27,3 +27,59 @@ let $3years = 3; //(Will show up as invalid so it's null you can use a $ sign, u
 
 
 //cant use reserved javascript keywords as variables they wil NOT work.
+/*
+Multi line comments is like this
+*/
+
+/*
+Variable mutation and type correcion
+*/
+
+//Type corcion
+var firstName = 'Travis';
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//variable mutation 
+
+age = 'twenty five';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
